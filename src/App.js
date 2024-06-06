e3977792945d4ab2cc70bfa083b3ca92cf727db7
akramchami99 <62516333+akramchami99@users.noreply.github.com>
2024-06-06 14:27:42 +0200

    App and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import ProductList from './Components/ProductList';
 import ProductDetail from './Components/ProductDetail';
 import Cart from './Components/Cart';
 import Login from './Components/Login';
+import { AuthProvider, AuthContext } from './Components/AuthContext';
 import './App.css';
 
 function App() {
@@ -12,11 +13,12 @@ function App() {
     <AuthProvider>
       <Router>
         <Navbar />
+        <Notification />
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/cart" element={<ProtectedRoute component={Cart} />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </Router>
@@ -24,5 +26,22 @@ function App() {
   );
 }
 
+function ProtectedRoute({ component: Component }) {
+  const { user } = useContext(AuthContext);
+
+  return user ? <Component /> : <div>Please log in to view this page.</div>;
+}
+
+function Notification() {
+  const { notification } = useContext(AuthContext);
+  
+  return (
+    notification ? (
+      <div className="notification">
+        {notification}
+      </div>
+    ) : null
+  );
+}
 
 export default App;
